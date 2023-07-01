f57cd5307354c140407be0df0448ed2242356fa4
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-07-01 20:54:25 +0530

    Create Script9.sh

---
+++ Script9.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+a=30
+if[a>20]
+then
+echo "Value of a is greater 20"
+elif[a>50]
+then
+echo "Value of a is greater than 50"
+elif[a>100]
+echo "Value of a is greater than 100"
+else
+echo "Value of a is less than 20"
+fi
